13cfe3a6ae159d8090684bd9893302106b12ff22
Michael McDonald <michael.mcdonald@10-248-240-248.wireless.oregonstate.edu>
2018-03-23 13:01:44 -0700

    messin with arrays

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function($) {
+
+ var myArray = [
+  "Apples",
+  "Bananas",
+  "Pears"
+];
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
   $("button").click(function(){
-      $("p").text("Hello world!");
+      $("p").text(randomItem);
   });
 });
